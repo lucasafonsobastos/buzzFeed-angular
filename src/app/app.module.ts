da2e362b9ae134d4ded7f4903c1a9a195bbb74ce
lucasafonsobastos <lucasafonsob@hotmail.com>
2023-05-16 10:42:02 -0300

    atualizando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { QizzComponent } from './components/qizz/qizz.component';
 import { CommonModule } from '@angular/common';
@@ -16,7 +18,8 @@ import { CommonModule } from '@angular/common';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
